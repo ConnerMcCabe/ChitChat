2879232a7bdb0415a9a177c957049bcb9685aaf0
Conner McCabe <conner.p.mccabe@gmail.com>
2020-09-16 10:47:42 -0500

    fixed error, page displays now

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 
 const app = express();
 
 //Set static folders
 app.use(express.static(path.join(__dirname, 'public')));
+//we learned today that for express find your public folder and run you need an index.js to get started,
+//landing page is always gonna be the index.js
+app.get('/', function (req, res) {
+  throw new Error('OOPS') // Express will catch this on its own.
+  
+})
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000 ;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
